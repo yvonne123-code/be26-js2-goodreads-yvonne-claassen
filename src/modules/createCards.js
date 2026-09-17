712f1c86faf290dccf3e7dacdbc91a30bb17f1ce
Yvonne C <yvonne.claassen@gritacademy.se>
2026-09-17 12:36:54 +0200

    fixed quick bug where adding ID to buttons went wrong

--- src/modules/createCards.js
+++ src/modules/createCards.js
@@ -35,15 +35,15 @@ export async function createCards(book){
     const deleteBtn = document.createElement('button');
     wrapper.append(deleteBtn);
     // add database ID to button for easy deletion
-    const deleteID = book.getFireBaseID(); 
-    deleteBtn.id = deleteID; 
+    // const deleteID = book.getFireBaseID(); 
+    // deleteBtn.id = deleteID; 
     deleteBtn.innerText = "Radera";
 
     const patchBtn = document.createElement('button');
     wrapper.append(patchBtn);
     // add database ID to button for easy patching to correct data
-    const patchID = book.getFireBaseID(); 
-    patchBtn.id = patchID;
+    // const patchID = book.getFireBaseID(); 
+    // patchBtn.id = patchID;
     patchBtn.innerText = "Markera som läst";
 
 } 
