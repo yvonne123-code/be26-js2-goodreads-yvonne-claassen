f241d146c2f693b18053313472dc442b551f393a
Yvonne C <yvonne.claassen@gritacademy.se>
2026-09-19 16:04:09 +0200

    patch: fixed css styling

--- src/modules/createCards.js
+++ src/modules/createCards.js
@@ -1,5 +1,5 @@
 // imports 
-import { createForm } from "./patch.js";
+import { createForm } from "./createForm.js";
 
 
 // variables
@@ -11,27 +11,41 @@ export async function createCards(book) {
 
     // createCards 
 
+    const card = document.createElement('div');
+    card.classList.add('col-12', 'col-md-6', 'col-lg-4', 'mb-4');
+    app.append(card);
+
     const wrapper = document.createElement('div');
-    wrapper.classList.add('wrapper');
-    app.append(wrapper);
+    wrapper.classList.add('card', 'h-100', 'shadow-sm', 'text-center');
+    card.append(wrapper);
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'p-2');
+    wrapper.append(cardBody);
 
     const ul = document.createElement('ul');
-    wrapper.append(ul);
+    ul.classList.add('list-group', 'list-group-flush', 'mb-3', 'text-center');
+    cardBody.append(ul);
 
     const title = document.createElement('li');
+    title.classList.add('list-group-item', 'fw-bold');
     ul.append(title);
     title.innerText = "Titel: " + book.getTitle();
 
     const author = document.createElement('li');
+    author.classList.add('list-group-item', 'text-muted');
     ul.append(author);
     author.innerText = "Författare: " + book.getAuthor();
 
     const publishYear = document.createElement('li');
+    publishYear.classList.add('list-group-item', 'text-muted');
     ul.append(publishYear);
     publishYear.innerText = "Publicerad: " + book.getPublishYear();
 
     const score = document.createElement('li');
+    score.classList.add('list-group-item', 'text-muted');
     ul.append(score);
+
     if (book.getScore() > 0 ){
         score.innerText = "Betyg: " + book.getScore();
     }
@@ -41,9 +55,9 @@ export async function createCards(book) {
 
     // buttons
     const deleteBtn = document.createElement('button');
-    wrapper.append(deleteBtn);
-    deleteBtn.classList.add('delete');
-    deleteBtn.innerText = "Radera";
+    cardBody.append(deleteBtn);
+    deleteBtn.classList.add('delete', 'd-block', 'mx-auto');
+    deleteBtn.innerText = "Radera Bok";
 
     deleteBtn.addEventListener('click', async () => {
         try {
@@ -57,7 +71,7 @@ export async function createCards(book) {
         }
     })
     if(book.getIsRead() === false){
-        const form = createForm(wrapper); 
+        const form = createForm(cardBody); 
         
         
         form.addEventListener('submit', async (event) => {
@@ -84,38 +98,6 @@ export async function createCards(book) {
         } )
     }
 
-
-    // if (book.getIsRead() === true && book.getScore() === 0) {
-
-    //     // request to score and send patch
-    //     createForm();
-
-
-    // }
-    // else if (book.getIsRead() === false) {
-    //     wrapper.append(patchBtn);
-    //     patchBtn.classList.add('patch');
-    //     patchBtn.innerText = "Markera som läst";
-
-    //     patchBtn.addEventListener('click', async () => {
-    //         try {
-    //             // console.log("clicked patch");
-    //             await book.patchBookRead();
-
-    //             patchBtn.remove();
-
-    //         }
-    //         catch (error) {
-    //             console.log(error);
-    //         }
-    //     })
-
-    // }
-    // else if (book.getIsRead() === true && book.getScore() != 0) {
-    //     // show score 
-    //     
-
-    // }
-
+    
 
 } 
---
+++ src/modules/createForm.js
@@ -0,0 +1,43 @@
+// imports 
+
+
+// functions 
+
+export function createForm(wrapper) {
+
+    const scoreForm = document.createElement('form');
+    scoreForm.classList.add('row', 'g-2', 'text-center', 'mt-2');
+    wrapper.append(scoreForm);
+
+    const div = document.createElement('div');
+    div.classList.add('col-12');
+    scoreForm.append(div);
+
+    const label = document.createElement('label');
+    label.innerText = "Ge ditt generella betyg från:";
+    label.classList.add('form-label', 'mb-0');
+    div.append(label);
+
+    const div2 = document.createElement('div');
+    div2.classList.add('col-12', 'd-flex', 'align-items-center', 'justify-content-center', 'gap-2');
+    scoreForm.append(div2);
+
+    const scoring = document.createElement('input');
+    scoring.type = 'number';
+    scoring.min = '1';
+    scoring.max = '10';
+    scoring.placeholder = '1-10';
+    scoring.required = true;
+    scoring.name = 'scoreInput'
+    scoring.classList.add('scoring', 'form-control', 'w-auto');
+    div2.append(scoring);
+
+    const btn = document.createElement('button');
+    btn.innerText = "Lägg till";
+    btn.classList.add('patch');
+    div2.append(btn);
+
+    return scoreForm; 
+}
+
+
